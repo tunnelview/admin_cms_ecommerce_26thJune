33b1b737eb8918da1c1804cf3d154ad742484490
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-06-26 12:05:57 +1000

    Add patch to rest.http, modify try/catch in registerLogin

--- api/src/models/adminUser/AdminUserModel.js
+++ api/src/models/adminUser/AdminUserModel.js
@@ -3,3 +3,7 @@ import AdminSchema from "./AdminUserSchema.js";
 export const crateNewAdmin = (obj) => {
   return AdminSchema(obj).save();
 };
+
+export const updateAdmin = (filter, obj) => {
+  return AdminSchema.findOneAndUpdate;
+};
