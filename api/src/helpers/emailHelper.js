8ffd3c2d2e5e54936132e1e39211d6a30db73f4d
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-06-26 10:23:44 +1000

    Add email configuration from nodemailer

---
+++ api/src/helpers/emailHelper.js
@@ -0,0 +1,40 @@
+import nodemailer from "nodemailer";
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendMail = (emailInfo)=> {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_SMTP,
+    port: +process.env.MAIL_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER, // generated ethereal user
+      pass: process.env.MAILID_PASS, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(emailInfo);
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+const sendAdminUserVerificationMail = (userObj) =>{
+    const emailInfo = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+    }
+
+    sendMail(emailInfo)
+}
